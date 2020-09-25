52fe15ee9176e4550db2ff77f7fb5cf1135a0332
Samvw90 <45743681+Samvw90@users.noreply.github.com>
2020-09-25 12:59:23 -0700

    Last Step 31

--- api/artist.js
+++ api/artist.js
@@ -37,4 +37,33 @@ artistRouter.get('/:artistId', (req, res, next) => {
   res.status(200).json({ artist: req.artist });
 });
 
+artistRouter.post('/', (req, res, next) => {
+  const { name, dateOfBirth, biography } = req.body.artist;
+  const isCurrentlyEmployed = req.body.artist.isCurrentlyEmployed === 0 ? 0 : 1;
+  if (!name || !dateOfBirth || !biography) {
+    res.status(400).send();
+  } else {
+    const sql =
+      'INSERT INTO Artist (name, date_of_birth, biography, is_currently_employed) VALUES ($name, $dateOfBirth, $biography, $isCurrentlyEmployed)';
+    const values = {
+      $name: name,
+      $dateOfBirth: dateOfBirth,
+      $biography: biography,
+      $isCurrentlyEmployed: isCurrentlyEmployed,
+    };
+    db.run(sql, values, function (err) {
+      if (err) {
+        next(err);
+      } else {
+        db.get(
+          `SELECT * FROM Artist WHERE id = ${this.lastID}`,
+          (err, artist) => {
+            res.status(201).json({ artist: artist });
+          }
+        );
+      }
+    });
+  }
+});
+
 module.exports = artistRouter;
