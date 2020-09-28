e793cc57e81002b05ba2799662546d13972e7e47
Samvw90 <45743681+Samvw90@users.noreply.github.com>
2020-09-28 11:19:16 -0700

    Step 43 done

---
+++ api/series.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const seriesRouter = express.Router();
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(
+  process.env.TEST_DATABASE || './database.sqlite'
+);
+
+seriesRouter.param('seriesId', (req, res, next, seriesId) => {
+  const sql = 'SELECT * FROM Series WHERE id = $seriesId';
+  const values = { $seriesId: seriesId };
+  db.get(sql, values, (err, series) => {
+    if (err) {
+      next(err);
+    } else if (series) {
+      req.series = series;
+      next();
+    } else {
+      res.status(404).send();
+    }
+  });
+});
+
+seriesRouter.get('/', (req, res, next) => {
+  db.all('SELECT * FROM Series', (err, series) => {
+    if (err) {
+      next(err);
+    } else {
+      res.status(200).json({ series: series });
+    }
+  });
+});
+
+seriesRouter.get('/:seriesId', (req, res, next) => {
+  res.status(200).json({ series: req.series });
+});
+
+seriesRouter.post('/', (req, res, next) => {
+  const { name, description } = req.body.series;
+  if (!name || !description) {
+    res.status(400).send();
+  } else {
+    const sql =
+      'INSERT INTO Series (name, description) VALUES ($name, $description)';
+    const values = {
+      $name: name,
+      $description: description,
+    };
+    db.run(sql, values, function (err) {
+      if (err) {
+        next(err);
+      } else {
+        db.get(
+          `SELECT * FROM Series WHERE id = ${this.lastID}`,
+          (err, series) => {
+            res.status(201).json({ series: series });
+          }
+        );
+      }
+    });
+  }
+});
+
+module.exports = seriesRouter;
